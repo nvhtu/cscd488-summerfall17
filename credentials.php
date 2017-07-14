e4b7dd35af7ea4fecc79ddb0259cb26466ea0000
nvhtu <hoangtu@live.com>
2017-07-14 16:35:12 -0700

    Replaced with pdoconfig.php

--- credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$serverName="localhost";
-	$user="root";
-	$pass="";
-	$database="ape_database";
-	
-	$connection = new PDO("mysql:host=$serverName;dbname=$database", $user, $pass);
-	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>

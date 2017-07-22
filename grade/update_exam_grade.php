3d462d1b234a2f8e7eff5d46b63b093c1d810f6a
Tu <hoangtu@live.com>
2017-07-22 13:14:56 -0700

    Finish update_exam_grade.php ver 1

---
+++ grade/update_exam_grade.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Update a student exam grade
+ * @author: Tu Nguyen
+ * @version: 1.0
+ */
+    //require "../pdoconfig.php";
+    require "../auth/user_auth.php";
+    require "../util/sql_exe.php";
+    require_once "check_examid_studentid.php";
+
+    
+    $requesterId = $_POST["requester_id"];
+    $requesterType = $_POST["requester_type"];
+    $allowedType = array("Admin", "Teacher");
+
+    $examId = $_POST["exam_id"];
+    $studentId = $_POST["student_id"];
+    $grade = $_POST["grade"];
+
+
+    //User authentication
+    user_auth($requesterId, $requesterType, $allowedType);
+
+    //Validate strings not empty
+
+    //Validate strings
+
+    checkExamExists($examId);
+    checkStudentExists($studentId);
+
+    //Add student exam grade
+    $sqlUpdateExamGrade = "UPDATE exam_grade
+                        SET grade = :grade
+                        WHERE student_id = :student_id";
+    
+    sqlExecute($sqlUpdateExamGrade, array('grade'=>$grade, 'student_id'=>$studentId), False);
+
+
+?>    

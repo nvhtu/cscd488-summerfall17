5ed0b068ea40b7ad085050d2ab70b172edc64815
Andrew Robinson <ydnabyrd@hotmail.com>
2017-07-24 18:05:37 -0700

    Added remove_location

---
+++ location/remove_location.php
@@ -0,0 +1,18 @@
+<?php
+	require_once "../util/sql_exe.php";
+	require_once "../auth/user_auth.php";
+	
+	$requesterId = $_POST["requester_id"];
+    $requesterType = $_POST["requester_type"];
+    $allowedType = array("Admin");
+	
+	//TODO: validate name and seats are set/valid
+	$id = $_POST["id"];
+	
+	//User authentication
+    user_auth($requesterId, $requesterType, $allowedType);
+	
+	sqlExecute("DELETE FROM location WHERE loc_id = :id",
+				array(':id' => $id),
+				false);
+?>

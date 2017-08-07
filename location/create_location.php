f95eb4cb2ed53456bfaa30fb38cf1c7c8e650380
Andrew Robinson <ydnabyrd@hotmail.com>
2017-08-07 15:08:22 -0700

    Added calls to input_validate functions

--- location/create_location.php
+++ location/create_location.php
@@ -1,14 +1,29 @@
 <?php
 	require_once "../util/sql_exe.php";
 	require_once "../auth/user_auth.php";
+	require_once "../util/input_validate.php";
 	
+	if(empty($_POST["requester_id"]) || empty($_POST["requester_type"]) || empty($_POST["name"]) || empty($_POST["seats"])){
+		var_dump(http_response_code(400));
+        die("Incomplete input.");
+	}
+
 	$requesterId = $_POST["requester_id"];
     $requesterType = $_POST["requester_type"];
     $allowedType = array("Admin", "Teacher");
 	
-	//TODO: validate name and seats are set/valid
 	$name = $_POST["name"];
 	$seats = $_POST["seats"];
+
+	//Sanitize the input
+	$requesterId = sanitize_input($requesterId);
+	$requesterType = sanitize_input($requesterType);
+	$name = sanitize_input($name);
+	$seats = sanitize_input($seats);
+
+	//Ensure input is well-formed
+	validate_only_numbers($seats);
+	validate_only_numbers($requesterId);
 	
 	//User authentication
     user_auth($requesterId, $requesterType, $allowedType);
--- location/update_location.php
+++ location/update_location.php
@@ -1,15 +1,33 @@
 <?php
 	require_once "../util/sql_exe.php";
 	require_once "../auth/user_auth.php";
+	require_once "../util/input_validate.php";
 	
+	if(empty($_POST["requester_id"]) || empty($_POST["requester_type"]) || 
+	empty($_POST["id"]) || empty($_POST["name"]) || empty($_POST["seats"])){
+		var_dump(http_response_code(400));
+        die("Incomplete input.");
+	}
+
 	$requesterId = $_POST["requester_id"];
     $requesterType = $_POST["requester_type"];
     $allowedType = array("Admin", "Teacher");
 	
-	//TODO: validate name, id, and seats are set/valid
 	$id = $_POST["id"];
 	$name = $_POST["name"];
 	$seats = $_POST["seats"];
+
+	//Sanitize the input
+	$requesterId = sanitize_input($requesterId);
+	$requesterType = sanitize_input($requesterType);
+	$id = sanitize_input($id);
+	$name = sanitize_input($name);
+	$seats = sanitize_input($seats);
+
+	//Ensure input is well-formed
+	validate_only_numbers($seats);
+	validate_only_numbers($id);
+	validate_only_numbers($requesterId);
 	
 	//User authentication
     user_auth($requesterId, $requesterType, $allowedType);

58358dbd82c47b0fe8ebee6e4964dce3a7d68dae
Andrew Robinson <ydnabyrd@hotmail.com>
2017-08-07 15:10:24 -0700

    Added calls to input_validate functions

--- category/get_all_categories.php
+++ category/get_all_categories.php
@@ -1,11 +1,24 @@
 <?php
 	require_once "../util/sql_exe.php";
 	require_once "../auth/user_auth.php";
+	require_once "../util/input_validate.php";
 	
+	if(empty($_POST["requester_id"]) || empty($_POST["requester_type"])){
+		var_dump(http_response_code(400));
+        die("Incomplete input.");
+	}
+
 	$requesterId = $_POST["requester_id"];
     $requesterType = $_POST["requester_type"];
     $allowedType = array("Admin", "Teacher", "Grader");
 	
+	//Sanitize the input
+	$requesterId = sanitize_input($requesterId);
+	$requesterType = sanitize_input($requesterType);
+
+	//Ensure input is well-formed
+	validate_only_numbers($requesterId);
+
 	//User authentication
     user_auth($requesterId, $requesterType, $allowedType);
 	
--- category/remove_category.php
+++ category/remove_category.php
@@ -1,14 +1,28 @@
 <?php
 	require_once "../util/sql_exe.php";
 	require_once "../auth/user_auth.php";
+	require_once "../util/input_validate.php";
 	
+	if(empty($_POST["requester_id"]) || empty($_POST["requester_type"]) || empty($_POST["id"])){
+		var_dump(http_response_code(400));
+        die("Incomplete input.");
+	}
+
 	$requesterId = $_POST["requester_id"];
     $requesterType = $_POST["requester_type"];
     $allowedType = array("Admin");
 	
-	//TODO: validate name and seats are set/valid
 	$id = $_POST["id"];
 	
+	//Sanitize the input
+	$requesterId = sanitize_input($requesterId);
+	$requesterType = sanitize_input($requesterType);
+	$id = sanitize_input($id);
+
+	//Ensure input is well-formed
+	validate_only_numbers($id);
+	validate_only_numbers($requesterId);
+
 	//User authentication
     user_auth($requesterId, $requesterType, $allowedType);
 	

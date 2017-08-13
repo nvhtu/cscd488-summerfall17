49b1406e0c3d1150ee5e02b8e4e17e4d1d86d3c0
Tu <hoangtu@live.com>
2017-08-13 12:41:54 -0700

    Add remove_ape.php

---
+++ ape/remove_ape.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Remove an exam
+ * @author: Tu Nguyen
+ * @version: 1.0
+ */
+    require_once "../auth/user_auth.php";
+    require_once "../util/sql_exe.php";
+
+    $requesterId = $_POST["requester_id"];
+    $requesterType = $_POST["requester_type"];
+    $allowedType = array("Admin", "Teacher");
+
+    //User authentication
+    user_auth($requesterId, $requesterType, $allowedType);
+
+    $examId = $_POST["exam_id"];
+
+    if(strcmp($requesterType, 'Teacher') == 0)
+    {
+        $sqlSelectId = "SELECT COUNT(*) as count
+                        FROM in_class_exam
+                        WHERE exam_id = :exam_id";
+        $data = array(':exam_id' => $exam_id);
+        $sqlResult = sqlExecute($sqlSelectId, $data, true);
+
+        if($sqlResult[0]["count"] == 0) {
+            var_dump(http_response_code(400));
+            die("Unauthorized access. Teachers can only delete their own in-class exam.");
+        }
+    }
+
+
+    $sqlDeleteExam = "DELETE FROM exam WHERE exam_id LIKE :id";
+    $data = array(':id' => $examId);
+
+    sqlExecute($sqlDeleteExam, $data, false);
+?>

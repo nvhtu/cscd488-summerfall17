7ba63114ad1a258f8b7325ab1a03813999bd5527
Andrew Robinson <ydnabyrd@hotmail.com>
2017-08-17 12:00:32 -0700

    Updated category script and modal files

--- view/category/category_script.js
+++ view/category/category_script.js
@@ -1,168 +1,168 @@
+
 $(document).ready(loaded);
 
-function loaded() {
-    //get all current categories and send to loadTable function,
-    $.get("../category/get_all_categories.php", 
-        {requester_id: "1",
-        requester_type: "Admin"}, 
-        loadTable,
-        "json");
+function loaded() 
+{
+    //Automatic GLOBAL variables
+    _userId = "111";
+    _userType = "Admin";
+    _userSessionId = "0";
+    
+    _targetModal = "detail-modal";
+    _tableId = "main-table";
+    _formId = "main-form";
 
-    //Set click handlers
-    $("#create-button").click(onClickCreate);
+    $("#requester-id").val(_userId);
+    $("#requester-type").val(_userType);
+    $("#requester-session").val(_userSessionId);
 
-    $("#submit-button").click(onClickSubmit);
+    getAllItems();
+
+    buildTable();
+
+    $("#create-button").click(onclickCreate);
+    $("#submit-button").click(submitForm);
 }
 
-//Adds json data to table using buildRow and appendRow helper functions
-function loadTable(data) {
+function buildTable()
+{
+    var headersArr = ["Name"];
+    var table = buildMainTable(headersArr);
+    $(".table-responsive").html(table);
+}
+
+function buildItemSummaryRow(item)
+{
+    var summaryData = {
+        id: item.cat_id,
+        name: item.name,
+    };
+
+    var row = buildItemRow(summaryData);
+
+    return row;
+}
+
+function loadTable(data) 
+{
     $.each(data, function(i, item) {
-        var row = buildRow(item);
-        appendRow(row);
+        var row = buildItemSummaryRow(item);
+        $(row).find(".btn-info").remove();
+
+        $("#" + _tableId).append(row);
     });
 }
 
-function buildRow(item) {
-    //create edit button
-    var $bttnEdit = $('<button type="button" name="edit" class="btn btn-info btn-sm" data-toggle="modal" data-target="#addCategoryModal"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span><span class="sr-only">Edit</span></button>');
-    $bttnEdit.attr("data-id", item.cat_id); //add unique ID from item as a data tag
-    $bttnEdit.click(onclickEdit);
-
-    //create delete button
-    var $bttnDel = $('<button type="button" name="delete" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span><span class="sr-only">Delete</span></button>');
-    $bttnDel.attr("data-id", item.cat_id);
-    $bttnDel.click(onclickDelete);
-
-
-    //wrap each piece of data in <td> tags, then wrap them all in a <tr> tag and return row
-    return $("<tr>")
-        .append(
-            $("<td>").text(item.name),
-            $("<td>").append($bttnEdit),
-            $("<td>").append($bttnDel)
-        );
-}
-
-//Append row to end of table
-function appendRow(row){
-    row.appendTo("#category-table");
-}
-
-//empties all inputs, including hidden id-input
-function onClickCreate() {
-    $("input").val("");
-}
-
-//sends category data to populateEdit function
-function onclickEdit(e) {
-    if(e.target.nodeName == "SPAN"){
-        //if user clicks on icon, get reference to button 
-        var btn = $(e.target).parent();
+function submitForm (e)
+{
+    if(e.currentTarget.dataset["action"] == "create")
+    {
+        createItem();
     }
-    else{
-        var btn = $(e.target);
+        
+    if(e.currentTarget.dataset["action"] == "update")
+    {
+        updateItem();
     }
-    $.get("../category/get_all_categories.php", 
-        {requester_id: "1",
-        requester_type: "Admin"}, 
-        function(data){
-            populateEdit(data, btn.data("id"));},
+        
+}
+
+function createItem()
+{
+    $.post("../category/create_category.php", $("#" + _formId).serialize(), function(lastInsertId){
+        $.get("../category/get_all_categories.php", 
+        {requester_id: _userId,
+        requester_type: _userType,
+        requester_session_id: _userSessionId,
+        cat_id: lastInsertId}, 
+        function(item){
+            loadTable(item);
+        },
         "json");
+    });
 }
 
-//Finds category record with matching cat_id and populates form with
-//data from that category
-function populateEdit(data, id) {
-    for(var i = 0; i < data.length; i++){
-        //check if cat_id equals the id from the edit button
-        if(data[i].cat_id == id){
-            $.each(data[i], function(key, value) {
-                $("[name=" + key + "]").val(value);
-            });
-        }
-    }
+function updateItem()
+{    
+    $.post("../category/update_category.php", $("#" + _formId).serialize(), function(){
+        var item = $("#" + _formId).serialize();
+        $.get("../category/get_all_categories.php", 
+        {requester_id: _userId,
+        requester_type: _userType,
+        requester_session_id: _userSessionId,
+        cat_id: $("#item-id").val()}, 
+        function(item){
+            var row = buildItemSummaryRow(item[0]);
+            $(row).find(".btn-info").remove();
 
-    //setTimeout forces this to wait till modal has opened
-    setTimeout(function(){
-        console.log($("input[name='name']")[0]);
-        $("input[name='name']")[0].focus();
+            $("tr[data-target='#item-" + item[0].cat_id + "']").replaceWith(row);
+        },
+        "json");
     }); 
 }
 
-//Prompts user to confirm delete, then deletes from database and removes
-//the row containing the clicked delete button
-function onclickDelete(e) {
-    if(window.confirm("Are you sure you want to delete this category?")){
-        if(e.target.nodeName == "SPAN"){
-            //if user clicks on icon, get reference to button
-            var btn = $(e.target).parent();
-        }
-        else{
-            var btn = $(e.target);
-        }
-
-        $.post("../category/remove_category.php", 
-            {requester_id: "1",
-            requester_type: "Admin",
-            id: btn.data("id")}, 
-            function(data) {
-                btn.parent().parent().remove();
-            });
-    }
+function onclickCreate()
+{
+    clearForm();
+    $(".modal-title").html("Create a Category");
+    $("#submit-button").attr("data-action", "create");
+    $("#submit-button").html("Create");
 }
 
-//Checks if this is an update or add action by seeing if the hidden 
-//id-input is empty or not, then calls appropriate function and hides form
-function onClickSubmit() {
-    if($("#id-input").val() === ""){
-        //Add new category
-        addCategory();
-    }
-    else{
-        //Update existing category
-        updateCategory();
-    }   
-}
+function onclickEdit(e) 
+{
+    clearForm();
+    var itemId = e.currentTarget.dataset["id"];
+    $("#item-id").val(e.currentTarget.dataset["id"]);
+    $(".modal-title").html("Edit a Category");
+    $("#submit-button").attr("data-action", "update");
+    $("#submit-button").html("Save changes");
 
-//Adds category to database and calls onSuccessfulAdd function
-function addCategory(){
-    $.post("../category/create_category.php", 
-        {requester_id: "1",
-        requester_type: "Admin",
-        name: $("input[name='name']").val()}, 
-        onSuccessfulAdd);
-}
-
-//Gets category data and creates and appends a row using the
-//last record in data (the record just added)
-function onSuccessfulAdd(){
     $.get("../category/get_all_categories.php", 
-        {requester_id: "1",
-        requester_type: "Admin"},
-        function(data){
-            var row = buildRow(data[data.length - 1]);
-            appendRow(row);
-        },
-        "json");
-}
-
-//Updates category with form data, then calls onSuccessfulUpdate function
-//with the name and data needed for row creation
-function updateCategory(){
-    var sendData = {requester_id: "1",
-        requester_type: "Admin",
-        name: $("input[name='name']").val(),
-        id: $("#id-input").val()};
-    $.post("../category/update_category.php", 
-        sendData, 
-        function(){
-            onSuccessfulUpdate({name: sendData.name, cat_id: sendData.id});
+    {requester_id: _userId,
+    requester_type: _userType,
+    requester_session_id: _userSessionId,
+    cat_id: itemId}, 
+    function(item){
+        $.each(item[0], function(name, val){
+            var el = $('[name="'+name+'"]');
+            el.val(val);
         });
+    },
+    "json");
+
 }
 
-//Creates updated row using passed in category data, then replaces the existing 
-//row containing the corresponding data-id with the updated version
-function onSuccessfulUpdate(item) {
-    var row = buildRow(item);
-    $("[name = edit][data-id =" + item.cat_id + "]").parent().parent().replaceWith(row);
+function onclickDelete(e) 
+{
+    if(window.confirm("Are you sure you want to delete this category?"))
+    {
+        var itemId = e.currentTarget.dataset["id"];
+        $.post("../category/remove_category.php", 
+        {requester_id: _userId,
+        requester_type: _userType,
+        requester_session_id: _userSessionId,
+        cat_id: itemId},
+        function(){
+            $("tr[data-target='#item-" + itemId + "']").remove();
+            $("tr[id='item-" + itemId + "']").remove();
+        });
+    }
+}
+
+function clearForm()
+{
+    $("#" + _formId).find("input[type=text], textarea").val(""); 
+}
+
+function getAllItems()
+{
+    $("#"+_tableId + " .item-row").empty();
+    
+    $.get("../category/get_all_categories.php", 
+        {requester_id: _userId,
+        requester_type: _userType,
+        requester_session_id: _userSessionId}, 
+        loadTable,
+        "json");
 }

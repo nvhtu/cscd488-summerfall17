fb52d8d7b112028ce6d85bc0f80a23b61e3e5e80
Tu <hoangtu@live.com>
2017-09-06 16:22:25 +0700

    Dynamic sidebar

--- util/get_cur_user_info.php
+++ util/get_cur_user_info.php
@@ -1,11 +1,44 @@
 <?php
-    session_start();
-    $userInfo = array('userId' => $_SESSION['ewuid'], 
-                      'userType' => $_SESSION["phpCAS"]["attributes"]["UserType"], 
-                      'userSession' => session_id(),
-                      'userFname' => $_SESSION["phpCAS"]["attributes"]["FirstName"],
-                      'userLname' => $_SESSION["phpCAS"]["attributes"]["LastName"],
-                      'userEmail' => $_SESSION["phpCAS"]["attributes"]["Email"] );
+
+    $isClient = $_GET["is_client"];
+    getCurUserInfo($isClient);
+
+    function getCurUserInfo($isClient)
+    {
+
+        $isDev = True;
+        $userInfo = array();
+
+        if($isDev)
+        {
+            $userInfo = array('userId' => '222', 
+            'userType' => 'Teacher', 
+            'userSession' => '111111',
+            'userFname' => 'Tu',
+            'userLname' => 'Nguyen',
+            'userEmail' => 'abc@xyz.com' );
+        }
+        else 
+        {
+            session_start();
+            $userInfo = array('userId' => $_SESSION['ewuid'], 
+                              'userType' => $_SESSION["phpCAS"]["attributes"]["UserType"], 
+                              'userSession' => session_id(),
+                              'userFname' => $_SESSION["phpCAS"]["attributes"]["FirstName"],
+                              'userLname' => $_SESSION["phpCAS"]["attributes"]["LastName"],
+                              'userEmail' => $_SESSION["phpCAS"]["attributes"]["Email"] );
+        }
     
-    echo json_encode($userInfo);
+        
+        if($isClient)
+        {
+            echo json_encode($userInfo);
+        }
+        else {
+            return $userInfo;
+        }
+        
+    }
+
+
 ?>
--- view/includes/sidebar.php
+++ view/includes/sidebar.php
@@ -1,5 +1,50 @@
-<li class="active"><a href="http://localhost/cscd488-summerfall17/view/home/index.php">APE Home <span class="sr-only">(current)</span></a></li>
-<li><a href="http://localhost/cscd488-summerfall17/view/exam/index.php">Exams</a></li>
-<li><a href="http://localhost/cscd488-summerfall17/view/category/index.php">Categories</a></li>
-<li><a href="http://localhost/cscd488-summerfall17/view/location/index.php">Locations</a></li>
-<li><a href="http://localhost/cscd488-summerfall17/view/user/index.php">Users</a></li>
+<?php
+
+    $_GET["is_client"] = False;
+    require_once "../../util/get_cur_user_info.php";
+
+    $userInfo = getCurUserInfo(False);
+
+    switch ($userInfo["userType"])
+    {
+        case "Admin": adminType();
+                        break;
+        case "Teacher": teacherType();
+                        break;
+        case "Grader": graderType();
+                        break;
+        case "Student": studentType();
+                        break;
+    }
+    
+
+    function adminType()
+    {
+        echo '<li class="active"><a href="http://localhost/cscd488-summerfall17/view/home/index.php">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/exam/index.php">Exams</a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/user/index.php">Users</a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/category/index.php">Categories</a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/location/index.php">Locations</a></li>';
+    }
+
+    function teacherType()
+    {
+        echo '<li class="active"><a href="http://localhost/cscd488-summerfall17/view/home/index.php">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/exam/index.php">Exams</a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/user/index.php">Students</a></li>';
+    }
+
+    function graderType()
+    {
+        echo '<li class="active"><a href="http://localhost/cscd488-summerfall17/view/home/index.php">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/exam/index.php">Grading</a></li>';
+    }
+
+    function studentType()
+    {
+        echo '<li class="active"><a href="http://localhost/cscd488-summerfall17/view/home/index.php">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="http://localhost/cscd488-summerfall17/view/exam/index.php">My Exams</a></li>';
+    }
+
+?>
+

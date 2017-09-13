24d964fc68f05283e146c0265111ff54dddefdf9
Tu <hoangtu@live.com>
2017-09-13 12:26:25 +0700

    Add "request" parameter

--- ape/get_all_apes.php
+++ ape/get_all_apes.php
@@ -9,19 +9,46 @@
 
     $requesterId = $_GET["requester_id"];
     $requesterType = $_GET["requester_type"];
+    $request = $_GET["request"];
     $allowedType = array("Admin", "Teacher", "Student");
 
     //User authentication
     user_auth($requesterId, $requesterType, $allowedType);
 
-    if (!empty($_GET["exam_id"]))
+
+
+    switch ($request)
+    {
+
+        case ("get_by_id"): getExamById();
+                            break;
+        case ("get_by_state"): getExamByState();
+                            break;
+        case ("get_all"): getAllExam($requesterType);
+                            break;
+        default: http_response_code(400);
+                echo "Unrecognized request string.";
+    }
+
+    function getExamById()
     {
         $sqlSelectExam = "SELECT *
-                            FROM exam
-                            WHERE exam_id = :exam_id";
+        FROM exam
+        WHERE exam_id = :exam_id";
         $sqlResult = sqlExecute($sqlSelectExam, array(":exam_id"=>$_GET["exam_id"]), true);
+        echo json_encode($sqlResult);
     }
-    else 
+
+    function getExamByState()
+    {
+        $sqlSelectExam = "SELECT *
+        FROM exam
+        WHERE state = :state";
+        $sqlResult = sqlExecute($sqlSelectExam, array(":state"=>$_GET["state"]), true);
+        echo json_encode($sqlResult);
+    }
+
+    function getAllExam($requesterType)
     {
         //If teacher, get only their exams
         if(strcmp($requesterType, 'Teacher') == 0)
@@ -36,9 +63,12 @@
         } else {
         $sqlResult = sqlExecute("SELECT * FROM exam", array(), true);
         }
+
+        echo json_encode($sqlResult);
     }
 
 
+
     
-	echo json_encode($sqlResult);
+	//echo json_encode($sqlResult);
 ?>

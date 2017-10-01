fab2d8ba37da6b8c27469a66323c1856e1654168
Tu <hoangtu@live.com>
2017-10-01 16:03:43 -0700

    Change My Exams page to non tabbed table

--- view/exam/index.php
+++ view/exam/index.php
@@ -1,8 +1,25 @@
 <?php 
+   require_once "../../util/get_cur_user_info.php";
+   $userInfo = getCurUserInfo(false);
    $page = "exam";
    $title = "EWU APE Exams";
    $tableTitle = "Exams";
-   $tableTabs = array("Open", "In Progress", "Grading", "Archived", "Hidden");
+   
    $hasModal = true;
+
+   if(strcmp($userInfo["userType"], "Student") == 0)
+   {
+        $tableTitle = "Exams History";
+   }
+   else 
+   {
+    $page = "exam";
+    $title = "EWU APE Exams";
+    $tableTitle = "Exams";
+    $tableTabs = array("Open", "In Progress", "Grading", "Archived", "Hidden");
+    $hasModal = true;
+   }
+
+   
    require_once "../index.php";
 ?>

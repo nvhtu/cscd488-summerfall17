77bcaff7a382dfab9e373306c084266c8a58464b
ydnabyrd <ydnabyrd@hotmail.com>
2017-10-04 01:51:02 -0700

    Fix grade index file

--- view/grade/index.php
+++ view/grade/index.php
@@ -1,7 +1,20 @@
 <?php 
-   $page = "grade";
-   $title = "EWU APE Grading";
-   $tableTitle = "Assigned APE Sections";
-   $hasModal = true;
-   require_once "../index.php";
+
+$_GET["is_client"] = False;
+require_once "../../util/get_cur_user_info.php";
+
+$userInfo = getCurUserInfo(False);
+
+if(strcmp($userInfo["userType"], "Admin") == 0 || strcmp($userInfo["userType"], "Teacher") == 0)
+{
+    $page = "grade";
+    $title = "EWU APE Grading";
+    $tableTitle = "Assigned APE Sections";
+    $hasModal = true;
+    require_once "../index.php";
+}
+else 
+{
+    die("Unauthorized access");
+}
 ?>

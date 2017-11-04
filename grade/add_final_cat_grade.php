11bc7e2864d81e2e078b089cd24e2c7c277d2920
nvhtu <hoangtu@live.com>
2017-11-03 18:23:34 -0700

    Add edited_by

--- grade/add_final_cat_grade.php
+++ grade/add_final_cat_grade.php
@@ -16,18 +16,19 @@
         $studentId = $_POST["student_id"];
         $examCatId = $_POST["exam_cat_id"];
         $finalGrade = $_POST["final_grade"];
+        $editedBy = $_POST["edited_by"];
 
-        updateStudentFinalCatGrade($studentId, $examCatId, $finalGrade);
+        updateStudentFinalCatGrade($studentId, $examCatId, $finalGrade, $editedBy);
 
     }
 
-    function updateStudentFinalCatGrade($studentId, $examCatId, $finalGrade)
+    function updateStudentFinalCatGrade($studentId, $examCatId, $finalGrade, $editedBy)
     {
-        $sqlInsertGrade = "INSERT INTO student_cat_grade (student_id, exam_cat_id, final_grade)
-                            VALUES (:student_id, :exam_cat_id, :final_grade)
-                            ON DUPLICATE KEY UPDATE final_grade = :final_grade";
+        $sqlInsertGrade = "INSERT INTO student_cat_grade (student_id, exam_cat_id, final_grade, edited_by)
+                            VALUES (:student_id, :exam_cat_id, :final_grade, :edited_by)
+                            ON DUPLICATE KEY UPDATE final_grade = :final_grade, edited_by = :edited_by";
 
-        $data = array(':student_id' => $studentId, ':exam_cat_id' => $examCatId, ':final_grade' => $finalGrade);
+        $data = array(':student_id' => $studentId, ':exam_cat_id' => $examCatId, ':final_grade' => $finalGrade, ':edited_by' => $editedBy);
         sqlExecute($sqlInsertGrade, $data, false);
     }
 ?>

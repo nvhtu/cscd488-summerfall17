51eeeb5d9b94f778c450cb73296554030f505521
aarongriffis <aarongriffis@hotmail.com>
2017-11-08 00:25:49 -0800

    Improve sidebar styling

--- view/index.php
+++ view/index.php
@@ -65,10 +65,8 @@
    
    <div class="container">
       <div class="row">
-         <div class="col-sm-3 col-md-2 sidebar">
-            <ul class="nav nav-sidebar">
-               <?php require_once $absPath . '/includes/sidebar.php'; ?>
-            </ul>
+         <div class="col-sm-3 col-md-2 sidebar list-group">
+            <?php require_once $absPath . '/includes/sidebar.php'; ?>
          </div>
 
          <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">

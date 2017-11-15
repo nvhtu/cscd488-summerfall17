9f717730b876c567376435f7d3c44e5c6943f51b
nvhtu <hoangtu@live.com>
2017-11-14 21:04:33 -0800

    Add send mail functionality.

--- ape/register.php
+++ ape/register.php
@@ -6,6 +6,7 @@
  */
 	require_once "../util/sql_exe.php";
 	require_once "../auth/user_auth.php";
+	require_once "../util/send_mail.php";
 	
 	$requesterId = $_POST["requester_id"];
     $requesterType = $_POST["requester_type"];
@@ -39,6 +40,25 @@
 				false);
 		//change student state to "Registered"
 		sqlExecute("UPDATE student SET state = :state WHERE student_id LIKE :id", array(":state" => "Registered", ":id" => $student_id), false);
+
+		$sqlGetStudentInfo = "SELECT f_name, l_name, email
+								FROM user
+								WHERE user_id LIKE :user_id";
+		$theStudentInfo = sqlExecute($sqlGetStudentInfo, array(":user_id"=>$student_id), true);
+		
+		$sqlGetExamInfo = "SELECT exam.name, DATE_FORMAT(date, '%m/%d/%Y') AS date, TIME_FORMAT(start_time, '%h:%i %p') AS start_time, location.name AS location_name
+							FROM exam JOIN location ON (location = loc_id)
+							WHERE exam.exam_id = :exam_id";
+		$theExamInfo = sqlExecute($sqlGetExamInfo, array(":exam_id"=>$exam_id), true);
+		
+		$mailSubject = "EWU APE Registration Confirmed";
+		$mailMsg = "You are now registered for the following exam:" .
+					"<br><br>" .
+					$theExamInfo[0]["name"] . " on " . $theExamInfo[0]["date"] . " at " . $theExamInfo[0]["start_time"] . " in " . $theExamInfo[0]["location_name"] .
+					"<br><br>" .
+					"Check https://ape.compsci.ewu.edu/ for more information.";
+		
+		sendMail($theStudentInfo[0], $mailSubject, $mailMsg);
 
 		echo json_encode($seatNum);
 	}

fb9602cff0ee202815184698d9446af33ceb9de6
nvhtu <hoangtu@live.com>
2017-12-04 13:13:42 -0800

    Comment out feedback section.

--- view/includes/sidebar.php
+++ view/includes/sidebar.php
@@ -61,10 +61,10 @@
         
     }
 
-    echo '<p class="list-group-item user-cat"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span>Feedback</p>
+    /*echo '<p class="list-group-item user-cat"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span>Feedback</p>
         <div class="list-group">
             <a class="list-group-item" href="mailto:tunguyen@eagles.ewu.edu?subject=Team 1 APE Website feedback"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Email the team</a>
-        </div>';
+        </div>';*/
     //"/home/index.php?page=admin_home" put a "page" variable into $_GET for view/index.php to display the correct requested home
 
     function adminType()
